53e58b0a61739ea3ecba1e09951450f38f3ca484
Luis Navarro <contacto@ing-luisnavarro.com.co>
2018-09-30 13:36:38 -0500

    Se conecto con socialite a Spotify

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+     public function redirectToProvider()
+        {
+            return Socialite::driver('github')->redirect();
+        }
+        public function redirectToProviderSpotify()
+        {
+            return Socialite::driver('spotify')->redirect();
+        }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+        {
+            $user = Socialite::driver('github')->user();
+            dd($user->token);
+            // $user->token;
+        }
+     public function handleProviderCallbackSpotify()
+        {
+            $user = Socialite::driver('spotify')->user();
+            dd($user->token);
+            // $user->token;
+        }
+
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
